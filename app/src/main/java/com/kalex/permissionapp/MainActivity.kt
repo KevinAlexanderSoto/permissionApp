1ffd58f117bda572b042efeca4441b3b40c7b68b
Kevin Soto <kevinalexandersoto999@gmail.com>
2023-06-29 16:11:09 -0500

    feature : add bluetooth permission

--- app/src/main/java/com/kalex/permissionapp/MainActivity.kt
+++ app/src/main/java/com/kalex/permissionapp/MainActivity.kt
@@ -3,19 +3,17 @@ package com.kalex.permissionapp
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import com.google.android.material.snackbar.Snackbar
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.kalex.permissionapp.databinding.ActivityMainBinding
 
@@ -49,12 +47,14 @@ class MainActivity : AppCompatActivity() {
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
     }
+
     private fun aksToPermission(REQUIRED_PERMISSIONS: Array<String>) {
         requestPermissionLauncher.launch(REQUIRED_PERMISSIONS)
     }
+
     private val requestPermissionLauncher =
         registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
+            ActivityResultContracts.RequestMultiplePermissions(),
         ) { isGranted: Map<String, Boolean> ->
             val permissionToAsk = mutableListOf<String>()
 
@@ -62,7 +62,7 @@ class MainActivity : AppCompatActivity() {
                 REQUIRED_PERMISSIONS.forEach {
                     when {
                         shouldShowRequestPermissionRationale(it) -> {
-                            //Usuario rechazo los permisos, pero se pueden pedir otra vez
+                            // Usuario rechazo los permisos, pero se pueden pedir otra vez
                             permissionToAsk.add(it)
                         }
                     }
@@ -72,34 +72,34 @@ class MainActivity : AppCompatActivity() {
                         aksToPermission(permissionToAsk.toTypedArray())
                     }
                 } else {
-                    //Usuario rechazo los permisos y marco la opcion de no mostrar otra vez
+                    // Usuario rechazo los permisos y marco la opcion de no mostrar otra vez
                     executeDialogForNegativePermission(false) {
-
                     }
                 }
-
             } else {
-                //acepto los permisos
+                // acepto los permisos
                 startFragment()
             }
         }
+
     private fun startFragment() {
         SecondFragment().show(supportFragmentManager, FRAGMENT_TAG)
     }
-    private fun executeDialogForNegativePermission(isRationale :Boolean,callback : ()->Unit){
-        //TODO: Add strings resources and style
+
+    private fun executeDialogForNegativePermission(isRationale: Boolean, callback: () -> Unit) {
+        // TODO: Add strings resources and style
         MaterialAlertDialogBuilder(this)
             .setTitle("ACEPTE LOS PERMISOS")
-            .setMessage("Si no los aceptas, no vas a poder tener mas perritos en la coleccion." +
-                    "Intenta ejecutar la camara otra vez, y dale al boton de ACEPTAR!!!!"
+            .setMessage(
+                "Quiero acceder a la camara para poder ESPIARTE, dale al boton ACEPTAR!!",
             )
-            .setPositiveButton("Vale, voy a reflexionar ") { dialog, _ ->
+            .setPositiveButton("Aceptar") { dialog, _ ->
                 callback.invoke()
-                if(!isRationale){
-                    //Take the User to the app settings
+                if (!isRationale) {
+                    // Take the User to the app settings
                     val intent = Intent(
                         Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                        Uri.fromParts("package", packageName, null)
+                        Uri.fromParts("package", packageName, null),
                     )
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                     startActivity(intent)
@@ -107,21 +107,27 @@ class MainActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
             .show()
-}
-
+    }
 
     companion object {
         private const val FRAGMENT_TAG = "second_fragment"
         private val REQUIRED_PERMISSIONS =
-            mutableListOf(
-                android.Manifest.permission.CAMERA,
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                mutableListOf(
+                    android.Manifest.permission.CAMERA,
+                    android.Manifest.permission.BLUETOOTH_SCAN,
+                    android.Manifest.permission.BLUETOOTH_CONNECT,
                 ).toTypedArray()
+            } else {
+                mutableListOf(
+                    android.Manifest.permission.CAMERA,
+                ).toTypedArray()
+            }
     }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) ||
+            super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/kalex/permissionapp/SecondFragment.kt
+++ app/src/main/java/com/kalex/permissionapp/SecondFragment.kt
@@ -1,11 +1,11 @@
 package com.kalex.permissionapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.kalex.permissionapp.databinding.FragmentSecondBinding
 
@@ -21,13 +21,12 @@ class SecondFragment : DialogFragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
-
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
